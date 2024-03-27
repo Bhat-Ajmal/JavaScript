83ff541eafb07070a65d780b587dfeb12fadb907
Ajmal Hussain Bhat <124758061+Bhat-Ajmal@users.noreply.github.com>
2024-03-27 12:52:52 +0530

    und. var

--- Basics/basics_variables.js
+++ Basics/basics_variables.js
@@ -1,1 +1,20 @@
-const accountId = 
+const accountId = 111
+let accountEmail = "ajmal@Gmail.com"
+var accountPassword = "123456"
+accountCity = "Srinagar"
+let accountState
+
+// accountId = 23   not allowed
+accountEmail = "ab@Google6.com"
+accountPassword = "12344321"
+accountCity = "Bengaluru"
+
+
+console.log(accountId)
+
+/*
+prefer not to use var, bcz of issue in block scope and unctional scope
+*/
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState])
